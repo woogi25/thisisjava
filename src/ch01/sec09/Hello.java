415f3251a34b508466e5e48a331a13ae16265aa9
woogi25 <hyunwook1891@gmail.com>
2022-11-27 18:54:32 +0900

    이것이 자바다 2.8까지 공부

---
+++ src/ch01/sec09/Hello.java
@@ -0,0 +1,23 @@
+package ch01.sec09; // 패키지 선언
+
+public class Hello { //public class Hello 클래스 선언 Hello는 클래스명
+
+	public static void main(String[] args) {
+		System.out.println("Hello, java");
+		System.out.println("Hello, /*주석이 될수 없음*/ welcome to the java world!");
+		int x;
+		x = 1;int y = 2;
+		int result = x + y;
+		System.out.println
+		(result);
+	}
+
+}
+// 행 주석
+/*범위 주석
+범위 주석*/
+/**********************************************
+
+
+도큐먼트 주석
+*****************************************/
---
+++ src/ch02/sec1/VaribleInitializationExample.java
@@ -0,0 +1,15 @@
+package ch02.sec1;
+
+public class VaribleInitializationExample {
+
+	public static void main(String[] args) {
+		//변수 value 선언
+		int value;
+		
+		//연산 결과를 변수 result의 초기값으로 대입
+//		int result = value + 10;
+//
+//		System.out.println(result);
+	}
+
+}
